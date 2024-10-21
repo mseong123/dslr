7fad5581fe4dd93bfec7d812f0b898a1e52fbb11
mseong <mseong123@hotmail.com>
2024-10-21 17:31:28 +0800

    mandatory nearly done

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,37 +2,51 @@
 import numpy as np
 
 class LogisticRegression():
-    '''Logistic Regression class'''
+    '''Logistic Regression class using one vs all (Sigmoid and binary cross entropy)'''
     def __init__(self, iterations:int = 1000, learning_rate:float = 0.001):
         self.iterations = iterations
         self.learning_rate = learning_rate
-    
-    def binary_cross_entropy_loss(y:np.ndarray, y_predict:np.ndarray)->np.ndarray:
+        self.weight = np.array([])
+        self.bias = np.array([])
+ 
+    def binary_cross_entropy_loss(self, y:np.ndarray, y_predict:np.ndarray)->np.ndarray:
         '''calculate and return loss value'''
         # shape is (size of sample,)
         return -((y * np.log(y_predict)) + ((1 - y) * (np.log(1 - y_predict))))
 
-    def sigmoid(z:np.ndarray)->np.ndarray:
+    def sigmoid(self, z:np.ndarray)->np.ndarray:
         '''sigmoid value between 0 and 1'''
         # shape is (size of sample,)
         return 1 / (1 + np.exp(-z))
-    
-    def gradient(y, y_predict,X:np.ndarray) ->tuple[np.ndarray]:
+
+    def gradient(self, y:np.ndarray, y_predict:np.ndarray,X:np.ndarray) ->tuple[np.ndarray]:
+        '''calculate gradient of weight and bias'''
         #dL/dy_predict = - ((y/y_predict) - (1-y/1-y_predict))
         # dy_predict/dz = y_predict * (1 - y_predict)
         # dz/dw 
         # gradient of loss w.r.t. dL/dw = (dL/dy_predict) x (dy_predict / dz) x (dz / dw)
         # simplify = ((y_predict - y) * X) / sample_size.
         # bias is the same except for dz/db = 1 hence (y_predict - y) / sample size
-        weights:np.ndarray = 
-        bias:np.ndarray = 
+        weight:np.ndarray = np.dot(y_predict - y, X.T) / len(y_predict)
+        bias:np.ndarray = np.mean(y_predict - y)
+        return (weight, bias)
 
-    
-    def fit(y:np.ndarray, X:np.ndarray)-> None:
+    def predict(self, X:np.ndarray) -> np.ndarray:
+        '''return probability of '''
+        z:float = np.dot(X, self.weight) + self.bias
+        return self.sigmoid(z)
+ 
+    def fit(self, y:np.ndarray, X:np.ndarray)-> None:
         '''train model based on data'''
         # weights = vector of features
-        weights:np.ndarray = np.zeros(X.shape[1])
-        bias:np.ndarray = np.zeros(1)
-        for 
-        
-    
+        self.weight:np.ndarray = np.zeros(X.shape[1])
+        self.bias:np.ndarray = np.zeros(1)
+        for i in range(self.iterations):
+            y_predict = self.predict(X)
+            loss = self.binary_cross_entropy_loss(y, y_predict)
+            if i % 100 == 0:
+                print(f"Iteration: {i}, Loss value: {loss}")
+            # GD Update step
+            gradient_weight, gradient_bias = self.gradient(y, y_predict, X)
+            self.weight += self.weight - (self.learning_rate * gradient_weight)
+            self.bias += self.bias - (self.learning_rate * gradient_bias)
--- logreg_train.py
+++ logreg_train.py
@@ -1,15 +1,36 @@
 '''Program to train model for classification of data to Hogsworth Houses (4 houses). Output is file for weights in logistic regression'''
 
-from logistic_regression import LogisticRegression
+import sys
 import numpy as np
 import pandas as pd
+from logistic_regression import LogisticRegression
 
-
-
+def filter_features(df, feature_to_filter) -> list[str]:
+    '''filter columns in df to be included in analysis'''
+    result:list[str] = df.columns.tolist()
+    def filter_feature(feature):
+        if feature in feature_to_filter:
+            return False
+        else:
+            return True
+    return list(filter(filter_feature, result))
 
 def main()-> None:
     '''Program input error checking and training model using Logistic Regression class'''
-    LR = LogisticRegression()
+    if (len(sys.argv) != 2):
+         raise ValueError("Incorrect No of arguments. Expected 1 argument which is training file")
+    try:
+        df = pd.read_csv(sys.argv[1])
+    except Exception as e:
+        print(f"File reading Error:{e}")
+    # From previous exercise, exclude the following features:
+    # 1) Defense Against the Dark Arts (since it is correlated with Astronomy)
+    # 2) Birthday, Best Hand, Arithmancy, Care of Magical Creatures since they have homogenous score distribution between the 4 houses
+    # model = LogisticRegression()
+    features_to_drop:list[str] = ["Index", "First Name", "Last Name", "Birthday", "Best Hand", "Arithmancy", "Care of Magical Creatures", "Defense Against the Dark Arts"]
+    
+    df = df.drop(features_to_drop, axis = 1)
+    
     
 
 
