aa54bd747e04a9c664e5caee707056c3c0f4b828
mseong <mseong123@hotmail.com>
2024-10-22 15:43:40 +0800

    training works, to wrap up for mandatory

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,20 +3,46 @@ import numpy as np
 
 class LogisticRegression():
     '''Logistic Regression class using one vs all (Sigmoid and binary cross entropy)'''
-    def __init__(self, iterations:int = 1000, learning_rate:float = 0.001):
-        self.iterations = iterations
-        self.learning_rate = learning_rate
-        self.weight = np.array([])
-        self.bias = np.array([])
+    def __init__(self, iterations:int = 1000, learning_rate:float = 0.1):
+        self._iterations = iterations
+        self._learning_rate = learning_rate
+        self._weight = np.array([])
+        self._bias = np.array([])
+    
+    @property
+    def weight(self) -> np.ndarray:
+        '''return weight of instance'''
+        return self._weight
+    
+    @property
+    def bias(self) -> np.ndarray:
+        '''return bias of instance'''
+        return self._bias
+
+    @weight.setter
+    def weight(self, value:np.ndarray) -> None:
+        '''setting weight of instance'''
+        self._weight = value
+    
+    @bias.setter
+    def bias(self, value:np.ndarray) -> None:
+        '''setting bias of instance'''
+        self._bias = value
+
  
     def binary_cross_entropy_loss(self, y:np.ndarray, y_predict:np.ndarray)->np.ndarray:
         '''calculate and return loss value'''
+        epsilon = 1e-10
         # shape is (size of sample,)
-        return -((y * np.log(y_predict)) + ((1 - y) * (np.log(1 - y_predict))))
+        # add epsilon to prevent log(0) which is undefined
+        return np.sum(-((y * np.log(y_predict + epsilon)) + ((1 - y) * (np.log(1 - y_predict + epsilon))))) / len(y_predict)
 
     def sigmoid(self, z:np.ndarray)->np.ndarray:
         '''sigmoid value between 0 and 1'''
         # shape is (size of sample,)
+        # print("max",np.max(z))
+        # print("min",np.min(z))
+        # set max and min z amount so doesn't overflow in sigmoid, this is for unnormalized data in predict
         return 1 / (1 + np.exp(-z))
 
     def gradient(self, y:np.ndarray, y_predict:np.ndarray,X:np.ndarray) ->tuple[np.ndarray]:
@@ -27,26 +53,35 @@ class LogisticRegression():
         # gradient of loss w.r.t. dL/dw = (dL/dy_predict) x (dy_predict / dz) x (dz / dw)
         # simplify = ((y_predict - y) * X) / sample_size.
         # bias is the same except for dz/db = 1 hence (y_predict - y) / sample size
-        weight:np.ndarray = np.dot(y_predict - y, X.T) / len(y_predict)
-        bias:np.ndarray = np.mean(y_predict - y)
-        return (weight, bias)
+        gradient_weight:np.ndarray = np.dot(X.T, y_predict - y) / len(y_predict)
+        gradient_bias:np.ndarray = np.mean(y_predict - y)
+        return (gradient_weight, gradient_bias)
 
     def predict(self, X:np.ndarray) -> np.ndarray:
-        '''return probability of '''
-        z:float = np.dot(X, self.weight) + self.bias
+        '''return probability of prediction based on data'''
+        z:float = np.dot(X, self._weight) + self._bias 
         return self.sigmoid(z)
+ 
+    def normalize(self, X:np.ndarray) -> np.ndarray:
+        '''convert each value to normalised Z score with mean of 0 and std deviation of 1'''
+        return ((X - np.mean(X,axis=0).reshape(1,-1)) / np.std(X,axis=0).reshape(1,-1)) 
  
     def fit(self, y:np.ndarray, X:np.ndarray)-> None:
         '''train model based on data'''
         # weights = vector of features
-        self.weight:np.ndarray = np.zeros(X.shape[1])
-        self.bias:np.ndarray = np.zeros(1)
-        for i in range(self.iterations):
-            y_predict = self.predict(X)
+        self._weight:np.ndarray = np.zeros(X.shape[1])
+        self._bias:np.ndarray = np.zeros(1)
+        X = self.normalize(X)
+        for i in range(self._iterations):
+            y_predict:float = self.predict(X)
             loss = self.binary_cross_entropy_loss(y, y_predict)
             if i % 100 == 0:
                 print(f"Iteration: {i}, Loss value: {loss}")
             # GD Update step
             gradient_weight, gradient_bias = self.gradient(y, y_predict, X)
-            self.weight += self.weight - (self.learning_rate * gradient_weight)
-            self.bias += self.bias - (self.learning_rate * gradient_bias)
+            # print("gradient weight", gradient_weight)
+            # print("gradient bias", gradient_bias)
+            # print("weight", self._weight)
+            # print("bias", self._bias)
+            self._weight -= self._learning_rate * gradient_weight
+            self._bias -= self._learning_rate * gradient_bias
--- logreg_train.py
+++ logreg_train.py
@@ -1,23 +1,36 @@
-'''Program to train model for classification of data to Hogsworth Houses (4 houses). Output is file for weights in logistic regression'''
+'''Program to train model for classification of data to Hogsworth Houses (4 houses). 
+Output is file for weights in logistic regression'''
 
 import sys
 import numpy as np
 import pandas as pd
 from logistic_regression import LogisticRegression
 
-def filter_features(df, feature_to_filter) -> list[str]:
-    '''filter columns in df to be included in analysis'''
-    result:list[str] = df.columns.tolist()
-    def filter_feature(feature):
-        if feature in feature_to_filter:
-            return False
-        else:
-            return True
-    return list(filter(filter_feature, result))
+def handle_missing_data(df:pd.DataFrame) -> pd.DataFrame:
+    '''convert NaN value into mean of the column'''
+    for i in range(len(df.columns)):
+        df[df.columns[i]] = df[df.columns[i]].fillna(value=np.mean(df[df.columns[i]]))
+    return df
+
+def process_label(df:pd.DataFrame) -> dict[str]:
+    """transform each classes to one vs all label """
+    y_ravenclaw:np.ndarray = df["Hogwarts House"].\
+        apply(lambda x:1 if x=="Ravenclaw" else 0).to_numpy()
+    y_slytherin:np.ndarray = df["Hogwarts House"].\
+        apply(lambda x:1 if x=="Slytherin" else 0).to_numpy()
+    y_gryffindor:np.ndarray = df["Hogwarts House"].\
+        apply(lambda x:1 if x=="Gryffindor" else 0).to_numpy()
+    y_hufflepuff:np.ndarray = df["Hogwarts House"].\
+        apply(lambda x:1 if x=="Hufflepuff" else 0).to_numpy()
+    return { "y_ravenclaw" : y_ravenclaw, "y_slytherin":y_slytherin, "y_gryffindor":y_gryffindor, "y_hufflepuff":y_hufflepuff}
+
+def normalize(X:np.ndarray) -> np.ndarray:
+        '''convert each value to normalised Z score with mean of 0 and std deviation of 1'''
+        return ((X - np.mean(X,axis=0).reshape(1,-1)) / np.std(X,axis=0).reshape(1,-1)) 
 
 def main()-> None:
     '''Program input error checking and training model using Logistic Regression class'''
-    if (len(sys.argv) != 2):
+    if len(sys.argv) != 2:
          raise ValueError("Incorrect No of arguments. Expected 1 argument which is training file")
     try:
         df = pd.read_csv(sys.argv[1])
@@ -27,12 +40,23 @@ def main()-> None:
     # 1) Defense Against the Dark Arts (since it is correlated with Astronomy)
     # 2) Birthday, Best Hand, Arithmancy, Care of Magical Creatures since they have homogenous score distribution between the 4 houses
     # model = LogisticRegression()
-    y_Ravenclaw:np.ndarray = df["Hogwarts House"].apply(lambda x: 1 if x=="Ravenclaw" else 0).to_numpy()
-    y_Slytherin:np.ndarray = df["Hogwarts House"].apply(lambda x: 1 if x=="Slytherin" else 0).to_numpy()
-    y_Gryffindor:np.ndarray = df["Hogwarts House"].apply(lambda x: 1 if x=="Gryffindor" else 0).to_numpy()
-    y_Hufflepuff:np.ndarray = df["Hogwarts House"].apply(lambda x: 1 if x=="Hufflepuff" else 0).to_numpy()
+    y_label:dict[str] = process_label(df)
     features_to_drop:list[str] = ["Index", "Hogwarts House", "First Name", "Last Name", "Birthday", "Best Hand", "Arithmancy", "Care of Magical Creatures", "Defense Against the Dark Arts"] 
-    df = df.drop(features_to_drop, axis = 1)
+    df:pd.DataFrame = df.drop(features_to_drop, axis = 1)
+    df:pd.DataFrame = handle_missing_data(df)
+    model_ravenclaw = LogisticRegression()
+    print("label", y_label["y_ravenclaw"])
+    model_ravenclaw.fit(y_label["y_ravenclaw"], df.to_numpy())
+    # print(df.loc[0:1].to_numpy())
+    result=model_ravenclaw.predict(normalize(df.loc[0:].to_numpy()))
+    np.savetxt("normalize.csv", normalize(df.loc[0:].to_numpy()), delimiter=",", fmt='%f')
+    np.savetxt("weight.csv", model_ravenclaw.weight, delimiter=",", fmt='%f')
+    np.savetxt("bias.csv", model_ravenclaw.bias, delimiter=",", fmt='%f')
+    print(np.std(df['Astronomy']))
+    np.savetxt("stddeviation.csv", [np.std(df['Astronomy'])], delimiter=",", fmt='%f')
+    np.savetxt("mean.csv", [np.mean(df['Astronomy'])], delimiter=",", fmt='%f')
+    # print(model_ravenclaw.weight)
+
     
     
 
